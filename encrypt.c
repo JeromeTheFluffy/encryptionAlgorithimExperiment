e6f4bc0ea6f09f4f3660334b5012418c7afe0197
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-04-29 13:01:10 -0400

    Update encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -1,7 +1,28 @@
+///////////////////////////////////////
+//this encrypts the file             //
+///////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
-typedef struct{
-  
+// convert string to int for encryption
+// pass it a pointer to char array
+int toInt(int ptrToString)
+{
+  // get size
+  int sizeOfString = strlen(&ptrToString);
+  // return variable
+  int encryptedInt;
+  //loop through string and convert to number
+  int i;
+  for(i=0;i<sizeOfString;i++)
+  {
+    // convert character to integer...
+    int convertedChar = (int)&ptrToString[i];
+    // and add it to the returned int 
+    encryptedInt += convertedChar;
+  }
+  return encryptedInt;
+}
