f62ec28595e55269c1c92af0b9708324a0815c1b
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-05-06 09:47:08 -0600

    Update encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -7,41 +7,40 @@
 #include <math.h>
 #include <string.h>
 
-// convert string to int for encryption
-// pass it a pointer to char array
-int integer = 0
+// globals
+time(timeStamp);
+int random = srand(&timeStamp);
 int address = &integer;
+FILE *data;
+// structure for key
+struct key{
+  int randomNum = srand(timeStamp);
+  int address = &randomNum;
+  int theTime timeStamp;
+  int total = randomNum + address + theTime;
+}
+// global for key
+struct key *encryptKey;
 
-FILE *randomAddress;
-
+// generate the key
 int generateRandomKey(int address)
 {
-  return int(address) * __time__ * 4 ^ 4 /7 + 0x0fab76a;
+  return (int(address) * timeStamp * 4) ^ 4 /7 + 0x0fab76a;
 }
-
-int toInt(int ptrToString)
+// make salt
+void writeKey(int theTimeString = encryptKey->theTime, int randomAddress = encryptKey->address, int randomNumber = encryptKey->randomNum, int total = encryptKey->total)
 {
-  // get size
-  int sizeOfString = strlen(&ptrToString);
-  // return variable
-  int encryptedInt;
-  //loop through string and convert to number
+  // init array
+  int everything[4];
+  // assign values to array
+  everything[0] = theTimeString;
+  everything[1] = randomAddress;
+  everything[2] = randomNumber;
+  everything[3] = total;
+  // write everything
   int i;
-  for(i=0;i<sizeOfString;i++)
+  for(i=0;i<4,i++)
   {
-    // convert character to integer...
-    int convertedChar = (int)&ptrToString[i];
-    // and add it to the returned int 
-    encryptedInt += convertedChar;
+    fwrite(everything[i],sizeof(int), 1, data)
   }
-  return encryptedInt;
-}
-// generate salt (multiple values
-int generateSalt()
-{
-  struct tm *seconds;
-  int seed = rand();
-  int theInt = 0;
-  theInt = theInt + seed^16 + ((2048 % 3) * 128) / 7;
-  return theInt;
 }
